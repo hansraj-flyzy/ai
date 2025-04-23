a669f7bb56a220b937c6599d820e8ece3dbcf7e7
Jeremy Morrell <jmorrell@cloudflare.com>
2025-04-23 08:53:16 -0700

    Sketch out updating this example for serving streamable and sse transports

--- demos/remote-mcp-server/src/index.ts
+++ demos/remote-mcp-server/src/index.ts
@@ -11,18 +11,38 @@ export class MyMCP extends McpAgent {
 	});
 
 	async init() {
-		this.server.tool("add", { a: z.number(), b: z.number() }, async ({ a, b }) => ({
-			content: [{ type: "text", text: String(a + b) }],
-		}));
+		this.server.tool(
+			"add",
+			{ a: z.number(), b: z.number() },
+			async ({ a, b }) => ({
+				content: [{ type: "text", text: String(a + b) }],
+			})
+		);
 	}
 }
 
 // Export the OAuth handler as the default
 export default new OAuthProvider({
-	apiRoute: "/sse",
-	// TODO: fix these types
-	// @ts-ignore
-	apiHandler: MyMCP.mount("/sse"),
+	apiRoute: ["/sse", "/mcp"],
+	apiHandler: {
+		// @ts-ignore
+		fetch(request: Request, env: Env, ctx: ExecutionContext) {
+			const url = new URL(request.url);
+
+			if (url.pathname === "/sse" || url.pathname === "/sse/message") {
+				// @ts-ignore
+				return MyMCP.serveSSE("/sse").fetch(request, env, ctx);
+			}
+
+			console.log("url.pathname", url.pathname);
+			if (url.pathname === "/mcp") {
+				// @ts-ignore
+				return MyMCP.serve("/mcp").fetch(request, env, ctx);
+			}
+
+			return new Response("Not found", { status: 404 });
+		},
+	},
 	// @ts-ignore
 	defaultHandler: app,
 	authorizeEndpoint: "/authorize",
